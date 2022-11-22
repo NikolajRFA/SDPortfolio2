16c2e570e41d042f93f326fc9515c81b357d57b9
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2022-11-22 13:33:19 +0100

    Added comments and tidied code.

--- src/AdjacencyGraph.java
+++ src/AdjacencyGraph.java
@@ -6,7 +6,7 @@ public class AdjacencyGraph {
     ArrayList<Vertex> vertices;
     HashMap<String, Integer> surplus;
     public AdjacencyGraph(){
-        vertices = new ArrayList<Vertex>();
+        vertices = new ArrayList<>();
     }
     public void addVertex(Vertex v){
         vertices.add(v);
@@ -20,10 +20,13 @@ public class AdjacencyGraph {
         {
             throw new Exception("Vertex is missing in vertices arraylist.");
         }
-        Edge newE=new Edge(from, to, weight);
+        new Edge(from, to, weight);
 
     }
 
+    /**
+     * Calculates the surplus of containers at each port, and adds this to a HashMap.
+     */
     public void calculateSurplus() {
         HashMap<String, Integer> surplus = new HashMap<>();
         for (Vertex vertex : vertices) {
@@ -45,31 +48,39 @@ public class AdjacencyGraph {
         return surplus;
     }
 
+    /**
+     * Finds the port in the surplus HashMap with the least containers in surplus.
+     * @return The Map.Entry with the least containers in surplus.
+     */
     public Map.Entry<String, Integer> findMinSurplus() {
         // Get min with the Java stream API.
         return surplus.entrySet().stream().min(Map.Entry.comparingByValue()).orElse(null);
     }
 
+    /**
+     * Finds the port in the surplus HashMap with the most containers in surplus.
+     * @return The Map.Entry with the most containers in surplus.
+     */
     public Map.Entry<String, Integer> findMaxSurplus() {
         // Get max with the Java stream API.
         return surplus.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
     }
 
+    /**
+     * Calculates which ports in the HashMap has to send how many containers to where, and the order of the shipments.
+     */
     public void minimumContainerFlow() {
-        System.out.println();
         if (surplus.size() == 1) return;
         Map.Entry<String, Integer> maxContainers = findMaxSurplus();
         Map.Entry<String, Integer> minContainers = findMinSurplus();
+        // Determine how to handle container shipment.
         if (minContainers.getValue() + maxContainers.getValue() > 0) {
-            System.out.println(surplus);
-
             System.out.println("Send " + (-1 * minContainers.getValue()) + " containers from " + maxContainers.getKey() + " to " + minContainers.getKey());
             totalCost += -1 * minContainers.getValue() * 100;
             maxContainers.setValue(maxContainers.getValue() + minContainers.getValue());
             surplus.remove(minContainers.getKey());
-        } else {
-            System.out.println(surplus);
 
+        } else {
             System.out.println("Send " + maxContainers.getValue() + " containers from " + maxContainers.getKey() + " to " + minContainers.getKey());
             totalCost += maxContainers.getValue() * 100;
             minContainers.setValue(minContainers.getValue() + maxContainers.getValue());
@@ -88,7 +99,7 @@ class Vertex {
     ArrayList<Edge> OutEdge;
     public Vertex(String name){
         this.name=name;
-        OutEdge=new ArrayList<Edge>();
+        OutEdge=new ArrayList<>();
     }
 
 }
